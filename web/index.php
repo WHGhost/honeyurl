068d239feb656d949c5188b05f728ec7ed6fc43f
WHGhost <wghosth@gmail.com>
2019-01-05 22:02:09 +0100

    Corrected a typo

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@ foreach($_POST as $key => $val){
 	$params .= "&" . $key . "=" . $val;
 }
 
-$https = !empty($SERVER['HTTPS']) && $_SERVER['HTTPS']==='on';
+$https = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']==='on';
 
 $query = $db->prepare("INSERT INTO requests (time, ip, host, method, useragent, params, https) VALUES (NOW(), ?, ?, ?, ?, ?, ?)");
 $query->execute(Array($_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_METHOD'], $_SERVER['HTTP_USER_AGENT'], $params, $https))
