f491306c9387fbcb8565bb1ad137cde2cfc56662
WHGhost <wghosth@gmail.com>
2019-01-05 21:29:06 +0100

    Added support for post variables and http host header

--- web/index.php
+++ web/index.php
@@ -8,8 +8,13 @@ $dbname = "honeyurl";
 $db = new PDO("mysql:host=$servname;dbname=$dbname", $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = $db->prepare("INSERT INTO requests (time, ip, method, useragent, params) VALUES (NOW(), ?, ?, ?, ?)");
-$query->execute(Array($_SERVER['REMOTE_ADDR'], $_SERVER['REQUEST_METHOD'], $_SERVER['HTTP_USER_AGENT'], $_SERVER['QUERY_STRING']))
+$params=$_SERVER['QUERY_STRING'];
+foreach($_POST as $key => $val){
+	$params .= "&" . $key . "=" . $val;
+}
+
+$query = $db->prepare("INSERT INTO requests (time, ip, host, method, useragent, params) VALUES (NOW(), ?, ?, ?, ?, ?)");
+$query->execute(Array($_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_METHOD'], $_SERVER['HTTP_USER_AGENT'], $params))
 
 
 
