57db8928090597913148ec7a50d1304582e023aa
WHGhost <wghosth@gmail.com>
2019-01-09 17:03:17 +0100

    Added safety message to cleanup script

--- clean_install.sh
+++ clean_install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+if [ $(id -u) != "0" ]
+then
+        echo "This script needs to be ran as root so it can delete the database."
+        exit
+fi
+
+echo "You are about to remove the entire database, continue ?"
+read -p 'y/N: ' input
+
+if [ $input != 'y' ]
+then
+	echo "Aborted"
+	exit 1
+fi
 mysql -e "SOURCE sql/clean_database.sql;"
